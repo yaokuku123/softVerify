0d683e9e3214af7da14d727e975a130eed02bf8b
ypwill <834287243@qq.com>
2021-10-09 15:38:04 +0800

    complete file upload

--- soft-certificate/src/main/java/com/ustb/softverify/controller/FileUploadController.java
+++ soft-certificate/src/main/java/com/ustb/softverify/controller/FileUploadController.java
@@ -35,10 +35,15 @@ public class FileUploadController {
         if (files.length != 2){
             return ResponseResult.error().message("文件个数错误");
         }
+        long size = files[0].getSize();
+        if (files[0].getSize() > 1024 * 1024 * 500 && files[1].getSize() > 1024 * 1024 * 500){
+            return ResponseResult.error().message("上传文件过大~");
+        }
         String soft = userUploadInfo.getSoftName();
-        String doc = userUploadInfo.getSoftName();
-        String softSuffix = soft.substring(soft.lastIndexOf("."));
-        String docSuffix = doc.substring(doc.lastIndexOf("."));
+        String originalFileName = files[0].getOriginalFilename();
+        String originalDocName = files[1].getOriginalFilename();
+        String softSuffix = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String docSuffix = originalDocName.substring(originalDocName.lastIndexOf("."));
 
         if (!".rar".equals(softSuffix) && !".zip".equals(softSuffix) ||!".xlsx".equals(docSuffix)){
             return ResponseResult.error().message("上传文件类型错误");
@@ -47,12 +52,15 @@ public class FileUploadController {
         User user = new User();
         SoftInfo softInfo = new SoftInfo();
         BeanUtils.copyProperties(userUploadInfo,user);
+        BeanUtils.copyProperties(userUploadInfo,softInfo);
         userService.insertUser(user);
         //保存文件
+        String softName = user.getUname() + "-" + soft + softSuffix;
+        String docName = user.getUname() + "-" + soft + docSuffix;
         String filePath = System.getProperty("user.dir") + "/data/" + user.getUname() + "/" + softInfo.getSoftName() + "/";
-        //FileUtil.saveFiles(userUploadInfo,filePath);
-        String softDestPath = filePath + user.getUname() + "/" + softInfo.getSoftName() + "/" + softSuffix;
-        String docDestPath = filePath + user.getUname() + "/" + softInfo.getSoftName() + "/" + docSuffix;
+
+        String softDestPath = filePath + softName;
+        String docDestPath = filePath + docName;
         File fileDir = new File(filePath);
         fileDir.mkdirs();
         File softDestFile = new File(softDestPath);
@@ -65,12 +73,10 @@ public class FileUploadController {
         }
 
         //存储软件相关信息
-        BeanUtils.copyProperties(userUploadInfo,softInfo);
-        softInfo.setSoftPath(filePath+soft).setDocPath(filePath+doc).setStatus(0).setUser(user);
+        softInfo.setSoftName(soft).setSoftPath(softDestPath).setDocPath(docDestPath).setStatus(0).setUser(user);
         softInfoService.insertSoft(softInfo);
 
         return ResponseResult.success().data("user",user).data("softInfo",softInfo);
     }
-
 
 }
