866d0791e389be5ab9dd3c6a909030fa638ea4ec
ypwill <834287243@qq.com>
2021-10-14 20:22:30 +0800

    add filetransferand upToChain test

--- soft-certificate/src/main/java/com/ustb/softverify/controller/FileUploadController.java
+++ soft-certificate/src/main/java/com/ustb/softverify/controller/FileUploadController.java
@@ -1,32 +1,15 @@
 package com.ustb.softverify.controller;
 
-
-
-import com.ustb.softverify.algorithm.sm3.SM3Algorithm;
 import com.ustb.softverify.domain.ResponseResult;
 import com.ustb.softverify.entity.dto.FileInfo;
-import com.ustb.softverify.entity.po.SignFile;
 import com.ustb.softverify.entity.po.SoftInfo;
 import com.ustb.softverify.entity.po.User;
 import com.ustb.softverify.entity.vo.UserUploadInfoVo;
-import com.ustb.softverify.service.Impl.ControlExcelImpl;
-import com.ustb.softverify.service.Impl.ZipCompressImpl;
 import com.ustb.softverify.service.SoftUploadService;
-import com.ustb.softverify.utils.FileUtil;
-import com.ustb.softverify.utils.HashBasicOperaterSetUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-
-import java.io.File;
-import java.io.IOException;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
 
 /**
  * @author WYP
@@ -37,14 +20,7 @@ import java.util.Map;
 public class FileUploadController {
 
     @Autowired
-    private ZipCompressImpl zipCompress;
-
-    @Autowired
-    private ControlExcelImpl controlExcel;
-
-    @Autowired
     private SoftUploadService softUploadService;
-
 
     @PostMapping("/upload")
     public ResponseResult upload(@RequestPart("files") MultipartFile[] files,@RequestPart("userUploadInfoVO") UserUploadInfoVo userUploadInfo) throws Exception {
@@ -68,7 +44,6 @@ public class FileUploadController {
         BeanUtils.copyProperties(userUploadInfo,softInfo);
 
         User user = softUploadService.insertUser(userUploadInfo);
-
         String softName = user.getGovUserId() + "-" + soft + softSuffix;
         String docName = user.getGovUserId() + "-" + soft + docSuffix;
         String filePath = System.getProperty("user.dir") + "/data/" + userUploadInfo.getGovUserId() + "/" + softInfo.getSoftName() + "/";
@@ -77,7 +52,6 @@ public class FileUploadController {
 
         softUploadService.saveFile(files,userUploadInfo,fileInfo);
         softUploadService.verifyAndSave(fileInfo,softInfo,userUploadInfo,user);
-
         return ResponseResult.success();
     }
 }
