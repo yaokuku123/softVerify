910dc12a889fb01a3dc2b88e28afd279f4c7981d
KangXi2333 <657721514@qq.com>
2021-10-19 23:03:01 +0800

    add scp method

--- soft-certificate/src/test/java/com/ustb/softverify/UtilsTest.java
+++ soft-certificate/src/test/java/com/ustb/softverify/UtilsTest.java
@@ -1,13 +1,24 @@
 package com.ustb.softverify;
 
 import com.ustb.softverify.entity.dto.CompInfo;
+import com.ustb.softverify.utils.HashBasicOperaterSetUtil;
 import com.ustb.softverify.utils.ReadTxt;
+import com.ustb.softverify.utils.ScpUtil;
+import com.ustb.softverify.utils.ZipDe;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ReadTxtTest {
+
+public class UtilsTest {
+
+    @Test
+    public void testZipDe() {
+        ZipDe zipDe = new ZipDe();
+        zipDe.unzipWithPassword("D:\\WORK\\TargetField\\test\\解压.zip", "D:\\WORK\\TargetField\\test\\UnZip", "123456");
+    }
+
     @Test
     public void testComp2txt() {
         ReadTxt readTxt = new ReadTxt();
@@ -15,7 +26,7 @@ public class ReadTxtTest {
         String filePath = "D:\\WORK\\TargetField\\CoreFiles.txt";
         String s1 = "java_error_in_webstorm64.hprof";
         String s2 = "1,155,720,249";
-        String s3 = "java_error_in_webstorm64.hprof1";
+        String s3 = "java_error_in_webstorm64.hprof";
         String s4 = "1,155,720,249";
         boolean status = false;
 
@@ -31,5 +42,17 @@ public class ReadTxtTest {
         status = readTxt.comp2txt(filePath, compInfoList);
 
         System.out.println(status);
+    }
+
+    @Test
+    public void scpTest() {
+        ScpUtil scpUtil = new ScpUtil();
+
+        try {
+            scpUtil.getFile("/root/scpTest/BAKA.txt", "D:\\WORK\\TargetField");
+            scpUtil.putFile("D:\\WORK\\TargetField\\CoreFiles.txt", "/root/scpTest");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
