9bd6364d6a8212d38c958378e5fd0e96b6a7098c
bianzhongshan <bianzhongshan1403@163.com>
2021-10-25 14:53:02 +0800

    modify Shannon Entropy

--- common/src/main/java/com/ustb/softverify/utils/ShannonEntropy.java
+++ common/src/main/java/com/ustb/softverify/utils/ShannonEntropy.java
@@ -1,17 +1,21 @@
-package edu.ustb.shannon;
+package com.ustb.softverify.utils;
 
 import java.io.FileInputStream;
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import static java.lang.Math.log;
 
 
+/**
+ * 计算二进制文件的香农熵 返回3byte的二进制文件 记录香农熵
+ */
 public class ShannonEntropy {
 
-    private static final int bufSize = 128;
+    private static final int bufSize = 512;
 
     /**
-     * 以8字节为缓冲区读取二进制文件，计算缓冲区内样本数量
+     * 以512字节为缓冲区读取二进制文件，计算缓冲区内样本数量
      */
     public static Map<String, Integer> byteFile2String(String fileName) throws Exception {
         Map<String, Integer> numberMap = new HashMap<>();
@@ -20,36 +24,31 @@ public class ShannonEntropy {
         int len;
         StringBuilder stringBuilder1 = new StringBuilder();
         String bitString;
-        String bitSubString;
-        int flag = 0;
         while ((len = in.read(buffer)) != -1) {
 
             if (len == bufSize) {
+
                 for (byte b : buffer) {
-                    flag++;
-                    if (flag == bufSize) {
-                        bitString = stringBuilder1.toString();
-                        calNumberOfSimple(numberMap, bitString);
-                        stringBuilder1 = stringBuilder1.delete(0, (bufSize - 2) * 8 + 1);
-                        flag = 0;
-                    } else {
-                        stringBuilder1.append(byte2String(b));
-                    }
+                    stringBuilder1.append(byte2String(b));
                 }
+                bitString = stringBuilder1.toString();
+                calNumberOfSimple(numberMap, bitString);
+                stringBuilder1 = stringBuilder1.delete(0, bitString.length() - 7);
+
             } else {
-                StringBuilder stringBuilder2 = new StringBuilder();
                 for (byte b : buffer) {
                     if (len == 0)
                         break;
-                    stringBuilder2.append(byte2String(b));
+                    stringBuilder1.append(byte2String(b));
                     len--;
                 }
-                bitString = stringBuilder2.toString();
+                bitString = stringBuilder1.toString();
                 calNumberOfSimple(numberMap, bitString);
             }
         }
         return numberMap;
     }
+
 
 
     /**
@@ -59,7 +58,7 @@ public class ShannonEntropy {
      */
     public static void calNumberOfSimple(Map<String, Integer> numberMap, String bitString) {
         String substring;
-        for (int i = 0; i < bitString.length() - 8; i++) {
+        for (int i = 0; i < bitString.length() - 7; i++) {
             substring = bitString.substring(i, i + 8);
             if (numberMap.containsKey(substring)) {
                 numberMap.put(substring, numberMap.get(substring) + 1);
@@ -70,7 +69,7 @@ public class ShannonEntropy {
     }
 
     /**
-     *  将一个字节的二进制文件转化成字符串
+     * 将一个字节的二进制文件转化成字符串
      */
     public static String byte2String(byte b) {
         String s = "";
@@ -94,13 +93,11 @@ public class ShannonEntropy {
             int numberOfSample = 0;
             for (String s : numberMap.keySet()) {
                 number = numberMap.get(s);
-                // System.out.println(integer);
                 numberOfSample += number;
             }
             for (String s : numberMap.keySet()) {
                 number = numberMap.get(s);
                 px = (double) number / numberOfSample;
-                // System.out.print(px + " ");
                 sum += (px * (log(px) / log(2)));
             }
             shanon = -sum;
@@ -109,39 +106,33 @@ public class ShannonEntropy {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        
-        // double2Bytes(shanon);
-        System.out.println(shanon);
 
-        int res28 = (int) (shanon  * 1024 * 1024 * 256 );
+        System.out.println("shannon: " + shanon);
+
+        int res28 = (int) (shanon * 1024 * 1024 * 256);
         int res16 = (int) (shanon * 64 * 1024);
         String binaryString16 = Integer.toBinaryString(res16);
         String binaryString28 = Integer.toBinaryString(res28);
 
-//        byte[] bytes = double2Bytes(shanon);
-//        StringBuilder stringBuilder = new StringBuilder();
-//        for (byte aByte : bytes) {
-//           stringBuilder.append(byte2String(aByte));
-//        }
-//        String s = stringBuilder.toString();
-//        System.out.println("shannon " + s);
-        System.out.println("binaryString16 " +binaryString16);
-        System.out.println("binaryString28 " +binaryString28);
-//        System.out.println("res28 " + res28);
-//        System.out.println("res16 " + res16);
+//        System.out.println("binaryString16 " + binaryString16);
+//        System.out.println("binaryString28 " + binaryString28);
 
-        String substring1 = binaryString16.substring(0, 8);
-        String substring2 = binaryString16.substring(9, 16);
+        String substring1 = binaryString28.substring(0, 8);
+        String substring2 = binaryString28.substring(8, 16);
+        String substring3 = binaryString28.substring(16, 24);
+
 
         byte b1 = bit2byte(substring1);
         byte b2 = bit2byte(substring2);
+        byte b3 = bit2byte(substring3);
 
-        byte[] res = new byte[]{b1,b2};
-        return  res;
+        byte[] res = new byte[]{b1, b2, b3};
+        return res;
     }
 
     /**
-     *  将double转化成byte[4]数组
+     * 将double转化成byte[4]数组
+     *
      * @param d
      * @return
      */
@@ -159,10 +150,10 @@ public class ShannonEntropy {
      * @param bString 二进制字符串
      * @return 对应的字节
      */
-    public static byte bit2byte(String bString){
-        byte result=0;
-        for(int i=bString.length()-1,j=0;i>=0;i--,j++){
-            result+=(Byte.parseByte(bString.charAt(i)+"")*Math.pow(2, j));
+    public static byte bit2byte(String bString) {
+        byte result = 0;
+        for (int i = bString.length() - 1, j = 0; i >= 0; i--, j++) {
+            result += (Byte.parseByte(bString.charAt(i) + "") * Math.pow(2, j));
         }
         return result;
     }
