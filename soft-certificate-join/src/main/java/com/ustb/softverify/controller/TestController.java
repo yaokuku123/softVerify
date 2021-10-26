fd896f3befee5b87bd138888ba10334eca863c38
yaokuku123 <yaoqijun@outlook.com>
2021-10-26 17:12:13 +0800

    添加访问前端的接口测试

--- soft-certificate-join/src/main/java/com/ustb/softverify/controller/TestController.java
+++ soft-certificate-join/src/main/java/com/ustb/softverify/controller/TestController.java
@@ -1,6 +1,10 @@
 package com.ustb.softverify.controller;
 
+import com.ustb.softverify.domain.ResponseResult;
+import com.ustb.softverify.entity.vo.ProjectVo;
 import com.ustb.softverify.entity.vo.UserVo;
+import com.ustb.softverify.service.UploadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,6 +17,9 @@ import java.io.IOException;
 @RequestMapping("/test")
 public class TestController {
 
+    @Autowired
+    private UploadService uploadService;
+
     @GetMapping("/visit")
     public void visit(@RequestParam("username") String username, HttpServletResponse resp) throws IOException {
         System.out.println(username);
@@ -24,6 +31,19 @@ public class TestController {
                                       HttpServletRequest req, HttpServletResponse resp) throws Exception {
         System.out.println(username);
         String url = "redirect:http://localhost:9527/#/soft/info?username="+username;
+        return new ModelAndView(url);
+    }
+
+    /**
+     * 接收全流程系统发送的数据
+     * @param projectVo
+     * @return
+     */
+    @PostMapping("/softwareaudit")
+    public ModelAndView softwareaudit2(@RequestBody ProjectVo projectVo){
+        ProjectVo project = uploadService.getResponseInfo(projectVo);
+        String appliedinst = project.getAppliedinst();
+        String url = "redirect:http://localhost:9527/#/soft/info?appliedinst="+appliedinst;
         return new ModelAndView(url);
     }
 }
